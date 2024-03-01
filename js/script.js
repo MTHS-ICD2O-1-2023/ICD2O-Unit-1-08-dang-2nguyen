eac7792a7babe49fde173f08e1ebdcc816d732d1
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-03-01 19:15:19 +0000

    qefqefwe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dang
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function myButtonClicked ()
+{
+  alert("Nguyen Hoang Thien Dang")
+}
